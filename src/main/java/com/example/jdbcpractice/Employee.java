7126309cfa58c958810b75abecc4a66ed8d764ab
ArtemBerezenko <a.a.berezenko@gmail.com>
2021-08-14 16:28:05 +0300

    solution example

--- src/main/java/com/example/jdbcpractice/Employee.java
+++
@@ -1,7 +0,0 @@
-package com.example.jdbcpractice;
-
-public class Employee {
-
-    public Employee() {
-    }
-}
--- src/main/java/com/example/jdbcpractice/JdbcPracticeApplication.java
+++ src/main/java/com/example/jdbcpractice/JdbcPracticeApplication.java
@@ -1,7 +1,9 @@
 package com.example.jdbcpractice;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.example.jdbcpractice.model.Employee;
+import com.example.jdbcpractice.service.FileLoader;
+import com.example.jdbcpractice.service.JDBCExecutor;
+import com.example.jdbcpractice.service.WorkStructureConverter;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,18 +19,12 @@ public class JdbcPracticeApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        //TODO: в args первым аргументом передать файл в котором будет находится список Employee
-        //TODO: сохранить новых Employee-в в соответстующиую таблицу
+        var fileLoader = new FileLoader();
+        List<Employee> employees = fileLoader.readFile("data/new-employees.csv");
         var jdbcExecutor = new JDBCExecutor();
-        jdbcExecutor.saveEmployees(List.of(new Employee()));
+        jdbcExecutor.saveEmployees(employees);
+        String employeeStr = WorkStructureConverter.convertToCsvString(jdbcExecutor.findEmployees());
+        fileLoader.writeToFile("data/all-employees.csv", employeeStr);
 
-        //TODO:Вернуть всех Employee-в из таблицы Employee
-        jdbcExecutor.findEmployees();
-        //TODO:Сохранить в исходный файл информацию по каждому департаменту
-        //TODO:Department: <название>
-        //TODO:Manager: <ФИО> <ЗП> и список его подчиненных
-        //TODO:Employee: <ФИО> <ЗП>
     }
-
-
 }
